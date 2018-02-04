569488449ae68259e7d8e335b86679c54a8c72cd
gourav goyal <gourav13041997@gmail.com>
2018-02-04 18:23:51 +0530

    ffff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port= require('./config')
 app.get('/',(req,res)=>{
-    res.send('hellll00000')
+     res.send('hellll00000')
 })
 
 app.listen(port, () => {
