84c7a988b4697115277926febd60e4dcef8cbb39
gourav goyal <gourav13041997@gmail.com>
2018-02-04 18:02:51 +0530

    hellllloooffdf
    Signed-off-by: gourav goyal <gourav13041997@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-
+const port= require('./config')
 app.get('/',(req,res)=>{
     res.send('hellll00000')
 })
 
-app.listen(2222, () => {
+app.listen(port, () => {
     console.log("Sever started on http://localhost:3434")
 })
